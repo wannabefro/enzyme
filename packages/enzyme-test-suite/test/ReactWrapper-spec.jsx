042d1fc2cc5aad407ed66d43cc52a74569ba9458
Jordan Harband <ljharb@gmail.com>
2018-10-04 15:18:49 -0700

    [Tests] add test for forwardRef + createRef
    See #1852

--- packages/enzyme-test-suite/test/ReactWrapper-spec.jsx
+++ packages/enzyme-test-suite/test/ReactWrapper-spec.jsx
@@ -343,6 +343,29 @@ describeWithDOM('mount', () => {
         expect(results).to.have.lengthOf(1);
         expect(results.props()).to.eql({ foo: 'hello' });
       });
+
+      it('actually passes the ref', () => {
+        class A extends React.PureComponent {
+          render () {
+            return <div>FOO</div>
+          }
+        }
+
+        class B extends React.PureComponent {
+          render () {
+            const { forwardedRef } = this.props;
+            return <A ref={forwardedRef} />
+          }
+        }
+
+        const BForwarded = forwardRef((props, ref) => (
+          <B {...props} forwardedRef={ref} />
+        ));
+
+        const ref = createRef();
+        mount(<BForwarded ref={ref} />);
+        expect(ref.current).to.be.an.instanceOf(A);
+      });
     });
 
     describeIf(is('> 0.13'), 'stateless function components (SFCs)', () => {
